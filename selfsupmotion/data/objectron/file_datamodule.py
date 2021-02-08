1f082a9b84783fe558a74da0285c1aec151ec7ef
Raphael Jean <raphael.jean@rocketmail.com>
2021-02-08 09:04:25 -0500

    Shuffle training set.

--- selfsupmotion/data/objectron/file_datamodule.py
+++ selfsupmotion/data/objectron/file_datamodule.py
@@ -291,7 +291,7 @@ class ObjectronFileDataModule(pytorch_lightning.LightningDataModule):
                 T.Normalize(*mean_std)
             ])
     def train_dataloader(self) -> DataLoader:
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
     
     def val_dataloader(self) -> DataLoader:
         return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
