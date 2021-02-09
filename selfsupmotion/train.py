3d663a43a9c4f342b12efb122997ea8ce7be7b76
Raphael Jean <raphael.jean@rocketmail.com>
2021-02-09 08:58:52 -0500

    Pairing next, early stop of val. accuracy.

--- selfsupmotion/train.py
+++ selfsupmotion/train.py
@@ -157,7 +157,7 @@ def train_impl(
         logger.info('starting training from scratch')
         resume_from_checkpoint = None
 
-    early_stopping = EarlyStopping("val_loss", mode="auto", patience=patience,
+    early_stopping = EarlyStopping("val_accuracy", mode="auto", patience=patience,
                                    verbose=use_progress_bar)
     trainer = pl.Trainer(
         # @@@@@@@@@@@ TODO check if we can add an online evaluator w/ callback
